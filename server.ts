d9bd7916fec88e0abfac6889e7dd60109f8c1e1f
Jaakko <jaakko.lipponen01@gmail.com>
2023-12-07 11:42:41 +0200

    changed port number

--- server.ts
+++ server.ts
@@ -1,6 +1,6 @@
 import express, { Express, Request, Response } from "express";
 const app: Express = express();
-const port: number = 5000;
+const port: number = 3000;
 app.use(express.json());
 
 // creating interfaces for all vehicles
